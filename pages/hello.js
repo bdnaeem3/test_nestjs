6b11e84e59179131f9ad94f357dd649633d910c8
Naeem Ahmed <bdnaeem3@gmail.com>
2025-08-12 18:38:59 +0600

    Checking Ideeza head for social share

---
+++ pages/hello.js
@@ -0,0 +1,55 @@
+import Head from "next/head";
+
+const Page = () => {
+  return (
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <script
+          async=""
+          src="https://platform.twitter.com/widgets.js"
+          charset="utf-8"
+        ></script>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          property="og:title"
+          content="Test Share Project - IDEEZA | AI Based SAAS"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:url" content="http://localhost:3200/project/5056" />
+        <meta
+          property="og:description"
+          content="Your One stop SAAS to Invent, Mint and Trade NFT’s of your very own Product design, Collaborate on product innovation and development, Find manufacturers and Purchase physical and virtual copies."
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ff00c7" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:image"
+          content="https://storage.googleapis.com/cdnideeza/devblob/media/5682.png?Expires=1755087617&amp;GoogleAccessId=bucket-access%40ideeza-ai.iam.gserviceaccount.com&amp;Signature=p1yC79de%2FGiThFzf4%2F0SaKxo7BIDb9L5yZROWdMNs4vyeEcCqjLaUXreLDwWXFfJdszteK0NB553wCRtsht3Xf1SV3p71ToSQY9CYlc0WB3eYM2ouGgMEgHPR1Alu9d9FslAdDjrQqjP7zWDD2NPfBiTurFwgU%2BHY6HQurWD%2BPY5x9ylDdKR5a5WCivkeFRxKhp21jhvuROBR%2FvpsmJx6%2ByN7f%2BFH1Cktx8e5cEoHZD80pnneQwUSriunLdYPP%2F0YSkfyYcMsxIeQf88zf1cYZyss3M9iiYnTpBgdNbz2MmF%2BEXSQgL7fAR9YlyYZGBA52WESClN6k21gbsRTwSotA%3D%3D"
+        />
+        <meta
+          name="description"
+          content="Your One stop SAAS to Invent, Mint and Trade NFT’s of your very own Product design, Collaborate on product innovation and development, Find manufacturers and Purchase physical and virtual copies."
+        />
+        <meta name="next-head-count" content="14" />
+        <meta name="theme-color" content="#ff00c7" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&amp;family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&amp;family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&amp;family=Montserrat:ital,wght@0,100..900;1,100..900&amp;display=swap"
+          rel="stylesheet"
+        />
+        <title>Test Share Project - IDEEZA | AI Based SAAS</title>
+      </Head>
+      Hello
+    </>
+  );
+};
+
+export default Page;
---
+++ pages/hello_2.js
@@ -0,0 +1,67 @@
+import Head from "next/head";
+
+const Page = () => {
+  return (
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <script
+          async=""
+          src="https://platform.twitter.com/widgets.js"
+          charset="utf-8"
+        ></script>
+        <meta charset="utf-8" />
+        <meta
+          property="og:title"
+          content="IDEEZA | AI Based SAAS - Project Details"
+        />
+        <meta property="og:type" content="" />
+        <meta property="og:image" content="" />
+        <meta property="og:image:width" content="" />
+        <meta property="og:image:height" content="" />
+        <meta property="og:url" content="" />
+        <meta
+          property="og:description"
+          content="Your One stop SAAS to Invent, Mint and Trade NFT’s of your very own Product design, Collaborate on product innovation and development, Find manufacturers and Purchase physical and virtual copies."
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          property="og:title"
+          content="Test Share Project - IDEEZA | AI Based SAAS"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta
+          property="og:description"
+          content="Your One stop SAAS to Invent, Mint and Trade NFT’s of your very own Product design, Collaborate on product innovation and development, Find manufacturers and Purchase physical and virtual copies."
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ff00c7" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:image"
+          content="https://storage.googleapis.com/cdnideeza/devblob/media/5682.png?Expires=1755086274&amp;GoogleAccessId=bucket-access%40ideeza-ai.iam.gserviceaccount.com&amp;Signature=n5ggvkqL8hJPcTJzyEjwa4dNvkalhx705RZc%2FSHHdQUzGi%2B8R%2B6PbZm2%2Bx2pTPKEri%2BswbNflFGqTVTAah8rPzlxC3UrlWJHs8PYRdR1V96dwoqE%2FrVfyYZwk19GU1ZgCG15cy6ttXNnDZ5aB5UDWF33RLnmLGSnSlrPRs%2B1hjOGGLrkb28Sg2X%2BkTd%2FP1ct0ulseiZDW8kMbRYwE1aCDZIlhovOgcvD1Syxgxo2J0cGUla7fHSmF61%2F9RXlBMOZ6QYavIKkJFgXM%2FMTq%2BnD4BeNNfP5ThG2VJEJj3E10FbppAIps%2By2HxQtl6928mKBziqlhy4NrmYk9QhsBD2Yyw%3D%3D"
+        />
+        <meta
+          property="og:url"
+          content="https://frontdev.ideeza.com/project/5056"
+        />
+        <meta name="next-head-count" content="21" />
+        <meta name="theme-color" content="#ff00c7" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          data-href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&amp;family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&amp;family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&amp;family=Montserrat:ital,wght@0,100..900;1,100..900&amp;display=swap"
+        />
+      </Head>
+      Hello 2
+    </>
+  );
+};
+
+export default Page;
